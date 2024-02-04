63a5a1294a342b89ea175ad99ca3203e2dc41baf
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-02-04 13:30:09 +0100

    Update user.jsx

--- src/pages/Profile/user.jsx
+++ src/pages/Profile/user.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom';
-import { Img, Text } from "components";
+import { Img, Text, Button } from "components";
 import { Link } from "react-router-dom";
 import { configureChains, createConfig, InjectedConnector, getAccount} from '@wagmi/core';
 import { publicProvider } from '@wagmi/core/providers/public';
@@ -54,7 +54,7 @@ const User = () => {
   const disconnectToWeb3 = () => {
       modal.open();
       modal.subscribeEvents(event => {
-        if (modal.close() && account.isConnected) {
+        if (!modal.open() && account.isConnected) {
           history('/signin');
         }
       });        
@@ -169,9 +169,8 @@ const User = () => {
               alt="arrowright_Three"
             />
           </Link>
-           <Link
+           <Button
             onClick={disconnectToWeb3}
-            to="/user"
             className="flex sm:flex-col flex-row sm:gap-5 items-start justify-start w-full"
           >
             <Img
@@ -189,7 +188,7 @@ const User = () => {
               src="images/img_arrowright_blue_gray_900_01.svg"
               alt="arrowright_Four"
             />
-          </Link>
+          </Button>
         </div>
       </div>
     </>
