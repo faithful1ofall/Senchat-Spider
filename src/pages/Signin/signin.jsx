28b6cd393d94b46ccfe329e858ffffc8f8b24cc1
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-02-07 17:04:46 +0100

    Update signin.jsx

--- src/pages/Signin/signin.jsx
+++ src/pages/Signin/signin.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import { Button, Img, Text } from "components";
-// import Header from "components/Header2/navbar";
-import Header from "components/Header/index";
+import Header from "components/Header2/navbar";
+// import Header from "components/Header/index";
 import { configureChains, createConfig, InjectedConnector, getAccount, readContract, watchAccount } from '@wagmi/core';
 import { publicProvider } from '@wagmi/core/providers/public';
 import { bscTestnet } from "viem/chains";
